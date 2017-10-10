91e4076b94d4c92da71a3d93828057c1b2dadbfb
Gaël ROTTIER <gael.rottier@smile.fr>
2017-10-10 09:59:56 +0200

    Fix startup script

--- container-files/replicate.sh
+++ container-files/replicate.sh
@@ -12,7 +12,7 @@ log() {
 
 sleep 5
 while true; do
-  for f in /var/lib/postgresql/data/pg_log/postgresql*.log; do 
+  for f in $PGDATA/pg_log/postgresql*.log; do 
     if [ -f $f ]; then
       if grep "database system is ready to accept connections" $f; then 
         break 2;
@@ -23,44 +23,53 @@ while true; do
   done;
 done
 
-log "Configurating PostgreSQL ${PG_MAJOR} BDR Cluster"
+log "Configuring PostgreSQL ${PG_MAJOR} BDR Cluster"
 
-oc config set-cluster http://kubernetes.default
+tablecount=$(psql -t -U $POSTGRES_USER $POSTGRES_DB -c "select count(*) from pg_tables where tableowner = '$POSTGRES_USER' and schemaname='bdr'")
 
-pod=$(oc get pod --selector=app=$APP_NAME --no-headers | grep -v $HOSTNAME | sort -R  | awk '$3 == "Running" {print $1;exit}')
-namespace=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
-service=$(oc get svc --selector=app=$APP_NAME --no-headers | awk '{print $1;exit}')
-host="${service}.${namespace}.svc.cluster.local"
-connectionString="port=5432 dbname=${POSTGRES_DB} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}"
+if [ "$tablecount" -ne 0 ]; then
 
-log "Creating extensions..."
-psql $POSTGRES_DB -U $POSTGRES_USER -c "
-  CREATE EXTENSION IF NOT EXISTS btree_gist;
-  CREATE EXTENSION IF NOT EXISTS bdr;"
-
-# First node created by StatefulSet, becomes first master
-# If $pod is not empty, other nodes exist, so it means the cluster is already set up
-if [ -z "$pod" -o "$HOSTNAME" == "$APP_NAME-0" ]; then
-
-  # First node creates the cluster
-  log "First node in the cluster, creating server group..."
-  psql $POSTGRES_DB -U $POSTGRES_USER -c "
-    SELECT bdr.bdr_group_create(
-      local_node_name := '${HOSTNAME}',
-      node_external_dsn := 'host=${HOSTNAME}.${host} ${connectionString}'
-    );"
+  log "Node restart, nothing to do..."
 
 else
 
-  log "The cluster already exists, joining server group..."
+  oc config set-cluster http://kubernetes.default
 
+  pod=$(oc get pod --selector=app=$APP_NAME --no-headers | grep -v $HOSTNAME | sort -R  | awk '$3 == "Running" {print $1;exit}')
+  namespace=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+  service=$(oc get svc --selector=app=$APP_NAME --no-headers | awk '{print $1;exit}')
+  host="${service}.${namespace}.svc.cluster.local"
+  connectionString="port=5432 dbname=${POSTGRES_DB} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}"
+
+  log "Creating extensions..."
   psql $POSTGRES_DB -U $POSTGRES_USER -c "
-    SELECT bdr.bdr_group_join(
-      local_node_name := '${HOSTNAME}',
-      node_external_dsn := 'host=${HOSTNAME}.${host} ${connectionString}',
-      join_using_dsn := 'host=${pod}.${host} ${connectionString}'
-    );"
+    CREATE EXTENSION IF NOT EXISTS btree_gist;
+    CREATE EXTENSION IF NOT EXISTS bdr;"
 
+  # First node created by StatefulSet, becomes first master
+  # If $pod is not empty, other nodes exist, so it means the cluster is already set up
+  if [ -z "$pod" -o "$HOSTNAME" == "$APP_NAME-0" ]; then
+
+    # First node creates the cluster
+    log "First node in the cluster, creating server group..."
+    psql $POSTGRES_DB -U $POSTGRES_USER -c "
+      SELECT bdr.bdr_group_create(
+        local_node_name := '${HOSTNAME}',
+        node_external_dsn := 'host=${HOSTNAME}.${host} ${connectionString}'
+      );"
+
+  else
+
+    log "The cluster already exists, joining server group..."
+
+    psql $POSTGRES_DB -U $POSTGRES_USER -c "
+      SELECT bdr.bdr_group_join(
+        local_node_name := '${HOSTNAME}',
+        node_external_dsn := 'host=${HOSTNAME}.${host} ${connectionString}',
+        join_using_dsn := 'host=${pod}.${host} ${connectionString}'
+      );"
+
+  fi
 fi
 
 log "Configuration done !"
